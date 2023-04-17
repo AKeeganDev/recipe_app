9f0272f80828ae4b76170c8a4e7bbe00985ab657
emyrue <94935633+emyrue@users.noreply.github.com>
2023-04-17 15:46:06 -0600

    Allow user to enter code to become admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   RECIPES_PER_PAGE = 4
-  
+
   def show
     @user = User.find(params[:id])
     @recipes_all = @user.recipes
@@ -10,5 +10,27 @@ class UsersController < ApplicationController
     @recipes = @recipes_all.offset((@current_page - 1) * RECIPES_PER_PAGE)
       .limit(RECIPES_PER_PAGE)
     @total_pages = (@recipes_all.count / RECIPES_PER_PAGE.to_f).ceil
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if user_params[:role] == ENV['SECRET_CODE']
+      @user.update(role: "admin")
+      @user.save
+      redirect_to '/'
+    else
+      flash[:error] = 'Incorrect code'
+      redirect_to edit_user_path(@user.id)
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:role)
   end
 end
