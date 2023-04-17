38b2e135dde5688180b607d25ab8c9fbd93feaab
Aaron Keegan <AKeegan.Dev@gmail.com>
2023-04-17 16:51:16 -0400

    add admin and user ability logic to app

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,21 +1,15 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     can :read, :all
 
-    if user.is? :admin
-      can :manage, Recipe
-    else
-      can :manage, Recipe, user: user
-    end
+    return unless user.present? # additional permissions for logged in users (they can read their own posts)
 
-    # return unless user.present?  # additional permissions for logged in users (they can read their own posts)
-    # can :manage, Recipe, user: user
+    can(:manage, Recipe, user:)
 
-    # return unless user.admin?  # additional permissions for administrators
-    # can :manage, Recipe
+    return unless user.admin? # additional permissions for administrators
+
+    can :manage, Recipe
   end
 end
